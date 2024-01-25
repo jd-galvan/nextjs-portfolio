277495ad297ecdff9d5debbf2990a9e4bcbfdf66
jd-galvan <jdgsuazo@gmail.com>
2024-01-25 16:51:35 -0500

    Adding blob storage

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   images: {
     remotePatterns: [{
+      hostname: 'lrkhaghcqdkei563.public.blob.vercel-storage.com',
+      protocol: "https"
+    },
+    {
       hostname: 'www.dropbox.com',
       protocol: "https"
     }]
