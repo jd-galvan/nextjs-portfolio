12fb2b2aa89aeef26801255c0c90b7ae45eb1e56
jd-galvan <jdgsuazo@gmail.com>
2024-01-25 02:40:32 -0500

    Implementing error and notfound page

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function error() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Oops! An error has occurred! :(</h2>
+    </div>
+  )
+}
+
+export default error
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>This page doesn't exist...</h2>
+    </div>
+  )
+}
+
+export default NotFound
